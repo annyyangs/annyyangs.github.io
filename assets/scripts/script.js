821ac1392e6e25dd2d9d5744ce90c16acffc6365
Anny Yang <annyyang@berkeley.edu>
2018-12-02 00:06:08 -0800

    Update script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,23 @@
 $(document).ready(function()  {
-	 $('.hidden').fadeIn(1000).removeClass('hidden');
+
+	$('.hidden').fadeIn(1000).removeClass('hidden');
+	$(".navbar").css({ 'animation' : '' });
+	
+	 
 	/* $(window).load(function() {
   	$("body").removeClass(".preload");
 	});*/
 
-	/* $(".navbar-item").click(function(){
-	 	$('.content').fadeOut(1000).addClass('hidden');
+	 $(".navbar-item").click(function(){
 
-	 })*/
+	 	let buttonName= $(this).text();
+
+	 	$('.content').fadeOut(1000);
+	 	
+
+	 })
+
+
 
 /*	 $(function(){
   	$(".navbar-item").bind("click",function(){
